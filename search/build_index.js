6cee85c40bb727be78abab290f7c0275f33d1808
idelem <idelem@163.com>
2020-10-06 15:28:32 +0800

    copy formatted link

--- search/build_index.js
+++ search/build_index.js
@@ -5,13 +5,13 @@ var cheerio = require("cheerio");
 
 
 // Change these constants to suit your needs
-const HTML_FOLDER = "..";  // folder with your HTML files
+const HTML_FOLDER = ".";  // folder with your HTML files
 // Valid search fields: "title", "description", "keywords", "body"
 const SEARCH_FIELDS = ["title", "body"];
 const EXCLUDE_DIRS = ["src", ".git", "node_modules"];
 const EXCLUDE_FILES = ["search.html"];
 const MAX_PREVIEW_CHARS = 275;  // Number of characters to show for a given search result
-const OUTPUT_INDEX = "lunr_index.js";  // Index file
+const OUTPUT_INDEX = "search/lunr_index.js";  // Index file
 
 
 function isHtml(filename) {
