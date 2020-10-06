210c5607e6e7976ebc9193fba7f1971d12f8b76e
idelem <idelem@163.com>
2020-10-06 14:22:41 +0800

    full text search and footer

--- prune.py
+++ prune.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 rootdir = '.'
@@ -5,7 +7,7 @@ rootdir = '.'
 def add_mobile_viewport_support(filepath):
     metastr = '    <meta name="viewport" content="width=device-width, initial-scale=1">\n'
     lines = []
-    with open(filepath, 'r') as f:
+    with open(filepath, 'r', encoding='UTF-8') as f:
         lines = f.readlines()
         if metastr in lines:
             return
@@ -13,19 +15,53 @@ def add_mobile_viewport_support(filepath):
             if lines[i].strip(" ").startswith("<meta"):
                 lines.insert(i, metastr)
                 break
-    with open(filepath, 'w') as f:
+    with open(filepath, 'w', encoding='UTF-8') as f:
         f.writelines(lines)
 
+def add_footer(filepath):
+    footerstr = """
+    <footer>
+        <p><a href="/search/search.html" target="_self">search</a> | <a href="https://github.com/idelem/trilium-garden" target="_blank">git repo</a></p>
+    </footer>\n"""
+
+    lines = []
+    newlines = []
+    skipping = False
+
+    with open(filepath, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            if "<footer>" in lines[i]:
+                skipping = True
+            if "</footer>" in lines[i]:
+                skipping = False
+                continue
+            if skipping:
+                continue
+            if lines[i].strip(" ").startswith("</body>"):
+                newlines.append(footerstr)
+            newlines.append(lines[i])
+
+    with open(filepath, 'w', encoding='UTF-8') as f:
+        f.writelines(newlines)
+
+exclude_dirs = [".git", "src", "search", "node_modules"]
+
+def is_excluded(subdir):
+    for dirname in exclude_dirs:
+        if dirname in subdir:
+            return True
 
 def main():
     for subdir, dirs, files in os.walk(rootdir):
-        if ".git" in subdir:
+        if is_excluded(subdir):
             continue
         for file in files:
             filepath = os.path.join(subdir, file)
-            print(filepath)
             if file.endswith(".html"):
+                print(filepath)
                 add_mobile_viewport_support(filepath)
+                add_footer(filepath)
 
 if __name__ == "__main__":
     main()
---
+++ search/lunrclient.js
@@ -0,0 +1,96 @@
+"use strict";
+
+var LUNR_CONFIG = {
+    "resultsElementId": "searchResults",  // Element to contain results
+    "countElementId": "resultCount"  // Element showing number of results
+};
+
+
+// Get URL arguments
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+// Parse search results into HTML
+function parseLunrResults(results) {
+    var html = [];
+    for (var i = 0; i < results.length; i++) {
+        var id = results[i]["ref"];
+        var item = PREVIEW_LOOKUP[id]
+        var title = item["t"];
+        var preview = item["p"];
+        var link = item["l"];
+        var result = ('<p><span class="result-title"><a href="../' + link + '">'
+                    + title + '</a></span><br><span class="result-preview">'
+                    + preview + '</span></p>');
+        html.push(result);
+    }
+    if (html.length) {
+        return html.join("");
+    }
+    else {
+        return "<p>Your search returned no results.</p>";
+    }
+}
+
+
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+}
+
+
+function showResultCount(query, total, domElementId) {
+    if (total == 0) {
+        return;
+    }
+
+    var s = "";
+    if (total > 1) {
+        s = "s";
+    }
+    var found = "<hr><br><p>Found " + total + " result" + s;
+    if (query != "" && query != null) {
+        query = escapeHtml(query);
+        var forQuery = ' for <span class="result-query">' + query + '</span>';
+    }
+    else {
+        var forQuery = "";
+    }
+    var element = document.getElementById(domElementId);
+    element.innerHTML = found + forQuery + "</p>";
+}
+
+
+function searchLunr(query) {
+    var idx = lunr.Index.load(LUNR_DATA);
+    // Write results to page
+    var results = idx.search(query);
+    var resultHtml = parseLunrResults(results);
+    var elementId = LUNR_CONFIG["resultsElementId"];
+    document.getElementById(elementId).innerHTML = resultHtml;
+
+    var count = results.length;
+    showResultCount(query, count, LUNR_CONFIG["countElementId"]);
+}
+
+
+// When the window loads, read query parameters and perform search
+window.onload = function() {
+    var query = getParameterByName("q");
+    if (query != "" && query != null) {
+        document.forms.lunrSearchForm.q.value = query;
+        searchLunr(query);
+    }
+};
